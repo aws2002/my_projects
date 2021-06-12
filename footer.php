55c29e33b4dd97eafab32196184f8d52e4f78cf9
Osama Husam <80280073+aws2002@users.noreply.github.com>
2021-06-12 19:56:07 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer>
+            <div class="text-center foter-tex mt-4">
+                    <p>All rights reserved <a href="">OSAMA HUSAM</a> ©2021 </p>
+            </div>
+
+
+
+</footer>
+
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
